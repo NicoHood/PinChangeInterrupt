063a17cf51a647841d6edc6542f84d4cf63b33f4
Nico <nico-2006@web.de>
2015-02-01 11:39:02 +0100

    #dev5 Added Uno definitions

--- PinChangeInterrupt.h
+++ PinChangeInterrupt.h
@@ -70,8 +70,8 @@ THE SOFTWARE.
 #if defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega88__)
 // some pins are not useable, we wont check this, since it doesnt matter as long as the user doesn't try to attach the individual PCINTs
 #define EXTERNAL_NUM_PINCHANGEINTERRUPT 24
-#define PCINT_INPUT0 PINB
-#define PCINT_INPUT1 PINC
+#define PCINT_INPUT0 (PINB & 0x3F) // 6, 7 Crystal
+#define PCINT_INPUT1 (PINC & 0x3F) // 14 Reset, 15 NC
 #define PCINT_INPUT2 PIND
 
 #elif defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega640__)
@@ -177,8 +177,49 @@ THE SOFTWARE.
 #define digitalPinToPinChangeInterruptOld(p) (digitalPinToPCICR(p) ? ((8 * digitalPinToPCICRbit(p)) + digitalPinToPCMSKbit(p)) : NOT_AN_INTERRUPT)
 
 // digital pin -> pcint number definitions
+// even if MISO etc are not PCINTs we have to map them to the pin and then refuse to a NOT_AN_INTERRUPT
+
+// Arduino Uno
+#define PIN_TO_PCINT_0 16
+#define PIN_TO_PCINT_1 17
+#define PIN_TO_PCINT_2 18
+#define PIN_TO_PCINT_3 19
 #define PIN_TO_PCINT_4 20
 #define PIN_TO_PCINT_5 21
+#define PIN_TO_PCINT_6 22
+#define PIN_TO_PCINT_7 23
+#define PIN_TO_PCINT_8 0
+#define PIN_TO_PCINT_9 1
+#define PIN_TO_PCINT_10 2
+#define PIN_TO_PCINT_11 3
+#define PIN_TO_PCINT_12 4
+#define PIN_TO_PCINT_13 5
+#define PIN_TO_PCINT_14 8
+#define PIN_TO_PCINT_15 9
+#define PIN_TO_PCINT_16 10
+#define PIN_TO_PCINT_17 11
+#define PIN_TO_PCINT_18 12
+#define PIN_TO_PCINT_19 13
+//#define PIN_TO_PCINT_20 NC
+//#define PIN_TO_PCINT_21 NC
+#define PIN_TO_PCINT_A0 PIN_TO_PCINT_14
+#define PIN_TO_PCINT_A1 PIN_TO_PCINT_15
+#define PIN_TO_PCINT_A2 PIN_TO_PCINT_16
+#define PIN_TO_PCINT_A3 PIN_TO_PCINT_17
+#define PIN_TO_PCINT_A4 PIN_TO_PCINT_18
+#define PIN_TO_PCINT_A5 PIN_TO_PCINT_19
+#define PIN_TO_PCINT_A6 PIN_TO_PCINT_20
+#define PIN_TO_PCINT_A7 PIN_TO_PCINT_21
+#define PIN_TO_PCINT_SS PIN_TO_PCINT_10
+#define PIN_TO_PCINT_MOSI PIN_TO_PCINT_11
+#define PIN_TO_PCINT_MISO PIN_TO_PCINT_12
+#define PIN_TO_PCINT_SCK PIN_TO_PCINT_13
+#define PIN_TO_PCINT_SDA PIN_TO_PCINT_18
+#define PIN_TO_PCINT_SCL PIN_TO_PCINT_19
+
+#define PIN_TO_PCINT_20 NOT_AN_INTERRUPT
+#define PIN_TO_PCINT_21 NOT_AN_INTERRUPT
+
 
 // creates a strong alias of a custom function to a user defined PCINT and function
 #define PinChangeInterruptStrongAlias(identifier, n) EXTERNC void pcint_callback_ptr_ ## n (void) __attribute__ ((alias (#identifier)))
