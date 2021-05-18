88c62ff31c9ced20e821423e51320b720e266e6e
NicoHood <NicoHood@users.noreply.github.com>
2021-05-18 17:20:46 +0200

    Added more Atmega 168, 88 variants

--- src/PinChangeInterruptBoards.h
+++ src/PinChangeInterruptBoards.h
@@ -30,7 +30,9 @@ THE SOFTWARE.
 
 // Microcontroller specific definitions
 
-#if defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328PB__) || defined(__AVR_ATmega168__) || defined(__AVR_ATmega88__) \
+#if defined(__AVR_ATmega328__) || defined(__AVR_ATmega328A__) || defined(__AVR_ATmega328PA__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328PB__) \
+   || defined(__AVR_ATmega168__) || defined(__AVR_ATmega168A__) || defined(__AVR_ATmega168PA__) || defined(__AVR_ATmega168P__) || defined(__AVR_ATmega168PB__) \
+   || defined(__AVR_ATmega88__) || defined(__AVR_ATmega88A__) || defined(__AVR_ATmega88PA__) || defined(__AVR_ATmega88P__) || defined(__AVR_ATmega88PB__) \
    || defined(__AVR_ATmega48__) || defined(__AVR_ATmega48A__) || defined(__AVR_ATmega48PA__) || defined(__AVR_ATmega48P__) || defined(__AVR_ATmega48PB__)
 // Arduino Uno
 #define PCINT_INPUT_PORT0 PINB
